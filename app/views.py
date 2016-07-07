c6a1a877e9d9c5bc5dd9d407c993aaab7a1b8d04
Lewis Kabui <lewis.maina@andela.com>
2016-07-07 20:09:27 +0300

    (feature) Include form context in search_user template

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 
 from app.forms import UserForm
 
@@ -9,5 +9,6 @@ def fetch_user(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
-            return HttpResponseRedirect('Searching...')
-    return render(request, 'app/search_user.html')
+            salute = 'Hi {0}!'.format(form.cleaned_data.get('username'))
+            return HttpResponse(salute)
+    return render(request, 'app/search_user.html', {'form': UserForm()})
