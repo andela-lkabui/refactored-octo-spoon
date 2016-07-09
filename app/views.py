8919461cca71632e535bb2d46fa80cc94bd34ff8
Lewis Kabui <lewis.maina@andela.com>
2016-07-09 18:22:32 +0300

    (feature) Use the Twitter class to display a user's recent tweets
    - Make use of the Twitter class to fetch the tweets and send a context
    for the results page to display

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from app.forms import UserForm
+from app.social import Twitter
 
 
 # Create your views here.
@@ -9,6 +10,12 @@ def fetch_user(request):
     if 'username' in request.GET:
         form = UserForm(request.GET)
         if form.is_valid():
-            salute = 'Hi {0}!'.format(form.cleaned_data.get('username'))
-            return HttpResponse(salute)
-    return render(request, 'app/search_page.html', {'form': UserForm()})
+            name = form.cleaned_data.get('username')
+            twitter = Twitter(name)
+            recent_tweets = twitter.save_tweets()
+            context = {
+                'tweets': recent_tweets,
+                'username': name
+            }
+            return render(request, 'app/results.html', context)
+    return render(request, 'app/search.html', {'form': UserForm()})
