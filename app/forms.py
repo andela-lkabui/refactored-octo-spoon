b8212e075606f0dd90aa39287fe2a705c74dbd38
Lewis Kabui <lewis.maina@andela.com>
2016-07-12 10:40:21 +0300

    (feature) Add datefield to form
    - Add a datefield to the form and set the field type to date so as to
    display a calendar on the front end for the user to enter the date

--- app/forms.py
+++ app/forms.py
@@ -3,3 +3,4 @@ from django import forms
 
 class UserForm(forms.Form):
     username = forms.CharField(label='Twitter Username', max_length=20)
+    since = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))
